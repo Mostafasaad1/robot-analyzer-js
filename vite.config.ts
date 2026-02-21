7c4bc0758adc44fd3381037483eecf6f4b7e2d8c
Mostafasaad1 <mostafa.saad@ejust.edu.eg>
2026-02-21 09:34:51 +0200

    ci: configure GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/robot-analyzer-js/',
   plugins: [react()],
   server: {
     port: 3000,
